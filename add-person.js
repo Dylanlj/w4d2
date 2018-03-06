90c61e8949025a634b6ba7deffa0b08cff52e3d5
Dylanlj <dylanl-j@hotmail.ca>
2018-03-06 22:51:26 +0000

    finished building queries with knex

---
+++ add-person.js
@@ -0,0 +1,22 @@
+const config = require ('./knexfile.js')
+const knex = require("knex")(config)
+
+function addPerson (people, callback){
+console.log(people)
+  knex('famous_people').insert([
+    {
+    first_name: people[0],
+    last_name: people[1],
+    birthdate: people[2]}
+  ])
+  .then(function(){
+    knex.destroy()
+  })
+  .catch(function(error){
+    console.error(error)
+  });
+
+}
+
+addPerson(process.argv.slice(2) )
+  
--- lookup_knex.js
+++ lookup_knex.js
@@ -1,54 +1,23 @@
-const settings = require("./settings");
-const knex = require("knex")({
-  client: 'pg',
-  connection: {
-    user     : settings.user,
-    password : settings.password,
-    database : settings.database,
-    host     : settings.hostname,
-    port     : settings.port,
-    ssl      : settings.ssl
-  }
-});
+const config = require ('./knexfile.js')
+const knex = require("knex")(config)
 
-
-function findPersonByName (client, people, callback){
-  const query = 
-  `SELECT * 
-  FROM famous_people 
-  where first_name = $1
-  or last_name = $1;`
-
-function findPersonByName (client, people, callback){
-  const query = 
-  `SELECT * 
-  FROM famous_people 
-  where first_name = $1
-  or last_name = $1;`
-
-  for(let i = 0;  i < people.length; i++){
-    client.query (query, [people[i]], (err, result) => {
-      if (err) {
-        console.error(err)
-        callback (err)
-        return
-      }
-     for(let p = 0; p < result.rows.length; p++){
-        callback(null, `${result.rows[p].first_name} ${result.rows[p].last_name}, born ${result.rows[p].birthdate.toDateString()}`)
-          if(i === people.length - 1){
-            client.end()
-          }
-        }
-      }
-    );
-  }
+function findPersonByName (people, callback){
+  knex('famous_people')
+  .where({first_name: people})
+  .orWhere({last_name: people})
+    .then( results => {
+      callback(null, results)
+    })
+    .catch( (err) => {
+      throw err
+    })
 }
 
-client.connect((err) => {
-  if (err){
-    return console.error("Connection Error", err);
+
+
+findPersonByName(process.argv[2], (err, results) => {
+  for(let row of results){
+    console.log(`${row.first_name} ${row.last_name}, born ${row.birthdate.toDateString()}`)
   }
-  findPersonByName(client, process.argv.slice(2), (err, result) => {
-    console.log(result)
-  })
+  knex.destroy()
 })
