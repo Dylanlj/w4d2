e0be3a070f0bc350bd01c35d9a9921413ac5de17
Dylanlj <dylanl-j@hotmail.ca>
2018-03-06 18:49:52 +0000

    rewrote code so that duplicate names will show up

--- lookup_knex.js
+++ lookup_knex.js
@@ -1,15 +1,23 @@
-const pg = require("pg");
 const settings = require("./settings");
-
-const client = new pg.Client({
-  user    : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+const knex = require("knex")({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
 });
 
+
+function findPersonByName (client, people, callback){
+  const query = 
+  `SELECT * 
+  FROM famous_people 
+  where first_name = $1
+  or last_name = $1;`
 
 function findPersonByName (client, people, callback){
   const query = 
